aaaf360e58611a5f4fd40812469b12f93b2b2aa3
P G Ayush Rai <vvce23cseaiml0100@vvce.ac.in>
2025-08-22 07:22:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,11 @@ const User = mongoose.model("User", userSchema);
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy",
-    "default-src 'self'; style-src 'self' 'unsafe-inline' https://unpkg.com; script-src 'self' 'unsafe-inline' https://unpkg.com;"
+    "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' https://cdn.tailwindcss.com;"
   );
   next();
 });
+
 
 
 // ------------------- AUTH ROUTES -------------------
